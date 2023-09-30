c95802371ec743ad12921795aaf17c3f1a117166
Conrado Andreu Capdevila <conrado.andreu@gmail.com>
2023-09-30 18:58:10 +0200

    config internal mercure << caddy to replace in subscription URLs

--- src/config/api.ts
+++ src/config/api.ts
@@ -8,3 +8,6 @@ export const GRAPHQL_ENTRYPOINT = AP_ENTRYPOINT + '/graphql';
 export const MERCURE_WELL_KNOWN = MERCURE_ENTRYPOINT + '/.well-known/mercure';
 
 export const MERCURE_TOPICS_PREFIX = "https://caddy.ap.orb.local";
+
+//  the URL that Caddy uses to contact Mercure
+export const CADDY_MERCURE_URL = "https://host.docker.internal:8445";
