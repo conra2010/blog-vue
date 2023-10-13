29cd5aea639902acd3112b6f6083db25a74159f0
Conrado Andreu Capdevila <conrado.andreu@gmail.com>
2023-10-13 21:52:40 +0200

    change order of home cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,5 @@
 <template>
-  <q-card dark bordered class="bg-grey-9 my-card">
-    <q-card-section>
-      <div class="text-h6">Posts</div>
-    </q-card-section>
-    <q-separator dark inset />
-    <q-card-section>
-      A virtual scroll showing several Post resources.
-    </q-card-section>
-  </q-card>
-  <q-card dark bordered class="bg-grey-9 my-card">
-    <q-card-section>
-      <div class="text-h6">SSE</div>
-    </q-card-section>
-    <q-separator dark inset />
-    <q-card-section>
-      A debug component to track Mercure events.
-    </q-card-section>
-  </q-card>
+  
   <q-card dark bordered class="bg-grey-9 my-card">
     <q-card-section>
       <div class="text-h6">About</div>
@@ -34,6 +17,24 @@
     <q-card-section>
       A single Vue component showing the same Post resource as 
       the previous one.
+    </q-card-section>
+  </q-card>
+  <q-card dark bordered class="bg-grey-9 my-card">
+    <q-card-section>
+      <div class="text-h6">Posts</div>
+    </q-card-section>
+    <q-separator dark inset />
+    <q-card-section>
+      A virtual scroll showing several Post resources.
+    </q-card-section>
+  </q-card>
+  <q-card dark bordered class="bg-grey-9 my-card">
+    <q-card-section>
+      <div class="text-h6">SSE</div>
+    </q-card-section>
+    <q-separator dark inset />
+    <q-card-section>
+      A debug component to track Mercure events.
     </q-card-section>
   </q-card>
 </template>
