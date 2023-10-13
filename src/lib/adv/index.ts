b3452bed635a3690fe43206a85fe6f384839fdb7
Conrado Andreu Capdevila <conrado.andreu@gmail.com>
2023-10-13 21:52:21 +0200

    needed to export netExchange

--- src/lib/adv/index.ts
+++ src/lib/adv/index.ts
@@ -1,8 +1,6 @@
 export { retryExchange } from './retryExchange';
 export type { RetryExchangeOptions } from './retryExchange';
 
-export { logExchange, logOpsExchange, otherExchange } from './logExchange';
-
+export { logExchange, logOpsExchange, otherExchange, netExchange } from './logExchange';
 export { cacheExchange } from './cacheExchange';
-
 export { fetchExchange } from './fetchExchange';
