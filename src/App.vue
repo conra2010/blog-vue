7611c2b465b5daefd1f922d15f8ed6938ce91e9a
Conrado Andreu Capdevila <conrado.andreu@gmail.com>
2023-10-13 21:51:48 +0200

    clean code and add some comments

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,26 @@
 import { RouterView } from 'vue-router'
 
 import { forwardSubscription } from '@/lib/urql'
-import { fetchExchange, logOpsExchange, retryExchange, logExchange, cacheExchange, otherExchange } from '@/lib/adv'
-// import { cacheExchange } from '@urql/exchange-graphcache'
+import { fetchExchange, retryExchange, logExchange, cacheExchange, netExchange } from '@/lib/adv'
 
 import { Client, mapExchange, provideClient, subscriptionExchange } from '@urql/vue';
-import { devtoolsExchange } from '@urql/devtools'
+
+// Only in Google Chrome ?
+//
+// import { devtoolsExchange } from '@urql/devtools'
 
 import { GRAPHQL_ENTRYPOINT } from '@/config/api';
 
 import { ref, watch } from 'vue'
-import { useNetwork, useDateFormat, useOnline, useEventBus, useTimeout } from '@vueuse/core'
+import { useOnline, useEventBus, useTimeout } from '@vueuse/core'
 
 import { useQuasar } from 'quasar'
-import { useCounterStore } from './stores/counter';
-import { pipe, toObservable } from 'wonka';
-import { valueFromAST } from 'graphql';
-import { netExchange, signalingExchange } from './lib/adv/logExchange';
-import { tiReload } from '@quasar/extras/themify';
 
-const { retry$, nextRetryString } = useCounterStore()
-
-const foo = pipe(retry$, 
-  toObservable)
-
-foo.subscribe({
-  next: (value) => console.log(value),
-  complete: () => {},
-  error: () => {}
-})
-
+//  check network status
+//  TODO check browser support
 const isOnline = useOnline()
 
+//  will provide a 'fetch' with a timeout option to urql
 async function fetchWithTimeout(
   url: RequestInfo | URL,
   opts: RequestInit | undefined,
@@ -54,8 +43,9 @@ async function fetchWithTimeout(
 const client = new Client({
   url: GRAPHQL_ENTRYPOINT,
   exchanges: [
-    //  Google Chrome has urql dev tools, uncomment this to send data to them
+    //  notifies an operation enters the chain, and a result exits the chain
     netExchange('head'),
+    //  console logging
     logExchange('head ', true),
     mapExchange({
       onError(error, operation) {
@@ -63,7 +53,8 @@ const client = new Client({
       }
     }),
     //otherExchange('foo bar'),
-    devtoolsExchange,
+    //  Google Chrome has urql dev tools, uncomment this to send data to them
+    // devtoolsExchange,
     logExchange('cache', true),
     cacheExchange,
     // cacheExchange({}),
@@ -72,11 +63,12 @@ const client = new Client({
       retryIf: (error) => { return true },
       maxNumberAttempts: 3
     }),
+    //  notifies a GraphQL Query operation gets here, a cache miss
     netExchange('fetch', { opf: (op) => { return (op.kind === 'query') } }),
     logExchange('fetch', true),
     fetchExchange,
-    //  see lib/urql.ts
     logExchange('subs ', true),
+    //  see lib/urql.ts
     subscriptionExchange({ forwardSubscription }),
     logExchange('tail ', true)
   ],
@@ -85,8 +77,10 @@ const client = new Client({
 
 provideClient(client);
 
+//  for notifications
 const $q = useQuasar()
 
+//  detects a change in network status
 watch(isOnline, () => {
   console.log('useOnline : ', isOnline.value)
   if (isOnline.value) {
@@ -96,14 +90,20 @@ watch(isOnline, () => {
   }
 })
 
+//  the urql exchanges above will notify into these some events
 const hbus = useEventBus<boolean>('head')
 const fbus = useEventBus<boolean>('fetch')
 
+//  use them to animate some UI indication of GraphQL activity
 const tick: number = 250
+//  an 'operation' gets into the urql client
 const { ready: op, start: oprx } = useTimeout(tick, { controls: true })
+//  a 'result' gets back from the urql client
 const { ready: rx, start: rxrx } = useTimeout(tick, { controls: true })
+//  a operation for a GraphQL Query (not a mutation, nor subscription) missed the urql cache
 const { ready: fx, start: fxrx } = useTimeout(tick, { controls: true })
 
+//  events start timeouts for the UI animations
 function hlistener(event: boolean) {
   if (event) {
     // console.log('[[ OP ]]')
@@ -121,9 +121,11 @@ function flistener(event: boolean) {
   }
 }
 
+//  subscribe to events
 const hsub = hbus.on(hlistener)
 const fsub = fbus.on(flistener)
 
+//  provide a 'refresh' UI button
 function handleReload() {
   location.reload()
 }
@@ -131,17 +133,19 @@ function handleReload() {
 </script>
 
 <template>
+  <!-- top layout -->
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-primary text-white" height-hint="98">
       <q-tabs align="left">
         <q-route-tab to="/" label="Home" />
-        <q-route-tab to="/ordered" label="Posts" />
-        <q-route-tab to="/sse" label="SSE" />
         <q-route-tab to="/about" label="About" />
         <q-route-tab to="/alt" label="Alt" />
+        <q-route-tab to="/ordered" label="Posts" />
+        <q-route-tab to="/sse" label="SSE" />
       </q-tabs>
     </q-header>
 
+    <!-- use Vue router and keep components alive  -->
     <q-page-container>
       <router-view v-slot="{ Component }">
         <keep-alive>
@@ -154,6 +158,7 @@ function handleReload() {
       <router-view></router-view>
     </q-page-container> -->
 
+    <!-- network status, GraphQL activity indicators, refresh button  -->
     <q-footer elevated class="bg-grey-8 text-white">
       <q-toolbar>
           <q-btn flat disable :label="isOnline ? 'online' : 'offline'" />
