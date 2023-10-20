dbea662ed32e6beee26408f701dd3eb98e6872d6
Conrado Andreu Capdevila <conrado.andreu@gmail.com>
2023-10-20 21:03:21 +0200

    example of triggering animation on DOM mutation

--- src/components/MercureEventSourceDebug.vue
+++ src/components/MercureEventSourceDebug.vue
@@ -4,14 +4,22 @@ import { useMercure, type MercureSource } from '@/lib/sse'
 
 import { onMounted } from 'vue';
 import anime from 'animejs'
+import { useMutationObserver } from '@vueuse/core';
+
+const el = ref()
 
 onMounted(() => {
-  anime({
-    targets: [ '.other' ],
-    duration: 2500,
-    translateX: '250px',
-    color: '#AA2'
-  })
+    const mut = useMutationObserver(el, (mutations) => {
+        // console.log(mutations[0].type)
+        if (mutations[0].type === 'characterData') {
+            anime({
+                targets: [ '.content' ],
+                rotate: '+=5',
+                duration: 500,
+                loop: false
+            })
+        }
+    }, { subtree: true, childList: true, characterData: true, attributes: true })
 })
 
 const props = defineProps<{ source: MercureSource }>()
@@ -54,7 +62,45 @@ const errorstr = computed(() => {
       <q-icon name="bluetooth" color="white" />
     </q-badge>
  -->
+    <div class="container">
+        <div class="content" style="scale: 0.50">
+<svg
+   width="100mm"
+   height="100mm"
+   viewBox="0 0 100 100"
+   version="1.1"
+   id="svg1"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs1" />
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1">
+    <rect
+       style="fill:none;fill-opacity:0.176166;stroke:#ffffff;stroke-width:1;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;opacity:0.3"
+       id="rect1"
+       width="99.009903"
+       height="99.009903"
+       x="0.49504951"
+       y="0.49504951" />
+    <path
+       style="opacity:0.3;fill:none;fill-opacity:0.176166;stroke:#ffffff;stroke-width:1;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"
+       d="M 25,50 H 75"
+       id="path1"
+       sodipodi:nodetypes="cc" />
+    <path
+       style="opacity:0.3;fill:none;fill-opacity:0.176166;stroke:#ffffff;stroke-width:1;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"
+       d="M 50,35 V 65"
+       id="path2"
+       sodipodi:nodetypes="cc" />
+  </g>
+</svg>
 
+            <!-- <p>Hello!</p> -->
+        </div>
+    </div>
     <q-card class="q-pa-none bg-grey-8 text-white ">
         <q-list dense>
             <q-item>
@@ -67,7 +113,7 @@ const errorstr = computed(() => {
                         Last Event ID:
                     </q-item-label>
                     <q-item-label>
-                        <q-badge>{{ lastEventID !== '' ? lastEventIDOnError : 'n/a' }}</q-badge>
+                        <q-badge ref="el" class="other">{{ lastEventID !== '' ? lastEventID : 'n/a' }}</q-badge>
                     </q-item-label>
                 </q-item-section>
             </q-item>
